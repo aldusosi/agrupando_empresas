a153bb92864945218dadd7efb095ef097c6aa2e6
aldusosi <desenho2510@gmail.com>
2021-08-12 08:29:43 -0300

    adicionado filtro de validações

---
+++ src/filtros/validaToken.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const knex = require('../conexao');
+
+async function validaToken (req, res, next){
+    const { authorization } = req.headers;
+
+    if(!authorization){
+        return res.status(400).json('O token não foi informado!');
+    }
+
+    try {
+        const token = authorization.replace('Bearer ', '').trim();
+        const { id } = jwt.verify(token, process.env.JWT_KEY);
+        const user = await knex('usuario').where('id', id);
+
+        if (user.length === 0) {
+            return res.status.json('Usuário não encontrado!');
+        }
+
+        const { senha, ...dadosUser } = user[0];
+        req.usuario = dadosUser;
+
+        next();
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+}
+
+module.exports = validaToken ;
--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cadastro = require('./controladores/usuarios/cadastro');
 const logar = require('./controladores/usuarios/login');
+const validaToken = require('./filtros/validaToken');
 
 const rotas = express();
 
 rotas.post('/cadastro', cadastro.cadastrarUsuario );
+rotas.use(validaToken);
 rotas.post('/login', logar.loginDeUsuario);
 
 module.exports = rotas;
