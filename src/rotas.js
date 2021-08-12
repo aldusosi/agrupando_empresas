34671d409df9510e6767c2db74f3ac33bcbe66c4
aldusosi <desenho2510@gmail.com>
2021-08-12 14:54:25 -0300

    implementada-a-função-de-listar-empresas

--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cadastro = require('./controladores/usuarios/cadastro');
 const logar = require('./controladores/usuarios/login');
+const listarEmpresa = require('./controladores/usuarios/listarEmpresas');
 const validaToken = require('./filtros/validaToken');
 
 const rotas = express();
@@ -10,5 +11,8 @@ rotas.post('/cadastro', cadastro.cadastrarUsuario );
 rotas.post('/login', logar.loginDeUsuario);
 
 rotas.use(validaToken);
+rotas.get('/empresas', listarEmpresa.listarEmpresas);
+
+
 
 module.exports = rotas;
